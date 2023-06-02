54fbf6c2e025747c6fcde1c03cd753b2a288aa29
Ubuntu <ubuntu@ip-172-31-7-94.us-west-2.compute.internal>
2023-06-02 05:26:48 +0000

    vectorized code working

--- ViT Implementation/model.py
+++ ViT Implementation/model.py
@@ -7,11 +7,17 @@ from utils import *
 # Code is adapted from: https://medium.com/mlearning-ai/vision-transformers-from-scratch-pytorch-a-step-by-step-guide-96c3313c2e0c
 # Credit goes to Brian Pulfer
 
+
 class ViT(nn.Module):
-
-    
-
-    def __init__(self, chw = (3, 32, 32), n_patches = 8, n_blocks=1, hidden_dim = 16, n_heads=2, n_classes=10):
+    def __init__(
+        self,
+        chw=(3, 32, 32),
+        n_patches=8,
+        n_blocks=1,
+        hidden_dim=16,
+        n_heads=2,
+        n_classes=10,
+    ):
         super(ViT, self).__init__()
         self.chw = chw
         self.n_patches = n_patches
@@ -20,55 +26,69 @@ class ViT(nn.Module):
         self.n_blocks = n_blocks
         self.n_heads = n_heads
 
-        assert chw[1] % n_patches == 0, "Input shape not entirely divisible by number of patches"
-        assert chw[2] % n_patches == 0, "Input shape not entirely divisible by number of patches"
+        assert (
+            chw[1] % n_patches == 0
+        ), "Input shape not entirely divisible by number of patches"
+        assert (
+            chw[2] % n_patches == 0
+        ), "Input shape not entirely divisible by number of patches"
 
         # Linear embedding step
-        input_dim = chw[0] * chw[1] * chw[2] // n_patches ** 2
+        input_dim = chw[0] * chw[1] * chw[2] // n_patches**2
         self.linear_embedding = nn.Linear(input_dim, hidden_dim)
 
         # Learnable classification token
         self.class_token = nn.Parameter(torch.randn(1, 1, hidden_dim))
 
         # Positional embedding
-        self.positional_embedding = nn.Parameter(torch.tensor(get_positional_embeddings(self.n_patches ** 2 + 1, self.hidden_dim)))
+        self.positional_embedding = nn.Parameter(
+            torch.tensor(
+                get_positional_embeddings(self.n_patches**2 + 1, self.hidden_dim)
+            )
+        )
         self.positional_embedding.requires_grad = False
 
-        # Transformer encoder blocks 
-        self.transformer_blocks = nn.Sequential(*[ViTBlock(hidden_dim, n_heads) for _ in range(n_blocks)])
+        # Transformer encoder blocks
+        self.transformer_blocks = nn.Sequential(
+            *[ViTBlock(hidden_dim, n_heads) for _ in range(n_blocks)]
+        )
 
         # MLP. Linear then softmax
         self.mlp = nn.Sequential(
             nn.Linear(hidden_dim, n_classes),
-            nn.Softmax(dim=-1), # softmax over the last element 
+            nn.Softmax(dim=-1),  # softmax over the last element
         )
 
-    
     # images size = [N, C, H, W]
     def forward(self, images):
-
         # convert the images to patches
-        patches = image_to_patches(images, self.n_patches) # (N, C, H, W) -> (N, n_patches^2, C * patch_size^2)
+        patches = image_to_patches(
+            images, self.n_patches
+        )  # (N, C, H, W) -> (N, n_patches^2, C * patch_size^2)
 
         # get the linear embedding
-        tokens = self.linear_embedding(patches) # (N, n_patches^2, hidden_dim)
+        tokens = self.linear_embedding(patches)  # (N, n_patches^2, hidden_dim)
 
-        # add the class token 
+        # add the class token
         # the purpose of this is to go from (N, n_patches^2, C * patch_size^2) -> (N, n_patches^2 + 1, C * patch_size^2)
         # we need to add a (1, 1, C * patch_size^2) tensor to the beginning of the patches tensor
-        tokens = torch.cat((self.class_token.repeat(patches.size(0), 1, 1), tokens), dim=1) # (N, n_patches^2 + 1, hidden_dim)
+        tokens = torch.cat(
+            (self.class_token.repeat(patches.size(0), 1, 1), tokens), dim=1
+        )  # (N, n_patches^2 + 1, hidden_dim)
 
         # add in the positional embedding
-        tokens += self.positional_embedding.repeat(patches.size(0), 1, 1) # (N, n_patches^2 + 1, hidden_dim)
+        tokens += self.positional_embedding.repeat(
+            patches.size(0), 1, 1
+        )  # (N, n_patches^2 + 1, hidden_dim)
 
         # pass through the transformer blocks
-        out = self.transformer_blocks(tokens) # (N, n_patches^2 + 1, hidden_dim)
+        out = self.transformer_blocks(tokens)  # (N, n_patches^2 + 1, hidden_dim)
 
         # take the class token out
-        class_tokens = out[:, 0, :] # (N, hidden_dim)
+        class_tokens = out[:, 0, :]  # (N, hidden_dim)
 
         # pass through the MLP
-        out = self.mlp(class_tokens) # (N, n_classes)
+        out = self.mlp(class_tokens)  # (N, n_classes)
 
         return out
 
@@ -86,15 +106,16 @@ class ViTBlock(nn.Module):
         self.mlp = nn.Sequential(
             nn.Linear(hidden_d, mlp_ratio * hidden_d),
             nn.GELU(),
-            nn.Linear(mlp_ratio * hidden_d, hidden_d)
+            nn.Linear(mlp_ratio * hidden_d, hidden_d),
         )
 
     def forward(self, x):
-        out = x + self.mhsa(self.norm1(x)) # residual connection 
-        out = out + self.mlp(self.norm2(out)) # residual connection
+        out = x + self.mhsa(self.norm1(x))  # residual connection
+        out = out + self.mlp(self.norm2(out))  # residual connection
         return out
-    
-# Multi-head self-attention module. 
+
+
+# Multi-head self-attention module.
 class MSA(nn.Module):
     def __init__(self, d, n_heads=2):
         super(MSA, self).__init__()
@@ -104,45 +125,60 @@ class MSA(nn.Module):
         assert d % n_heads == 0, f"Can't divide dimension {d} into {n_heads} heads"
 
         d_head = int(d / n_heads)
-        self.q_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-        self.k_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-        self.v_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.q_mapping = nn.Linear(d, d)  # change to output the same dimension
+        self.k_mapping = nn.Linear(d, d)  # change to output the same dimension
+        self.v_mapping = nn.Linear(d, d)  # change to output the same dimension
         self.d_head = d_head
         self.softmax = nn.Softmax(dim=-1)
 
+    # def forward(self, sequences):
+    #     # Sequences has shape (N, seq_length, token_dim)
+    #     # We go into shape    (N, seq_length, n_heads, token_dim / n_heads)
+    #     # And come back to    (N, seq_length, item_dim)  (through concatenation)
+    #     result = []
+    #     for sequence in sequences:
+    #         seq_result = []
+    #         for head in range(self.n_heads):
+    #             q_mapping = self.q_mappings[head]
+    #             k_mapping = self.k_mappings[head]
+    #             v_mapping = self.v_mappings[head]
+
+    #             seq = sequence[:, head * self.d_head : (head + 1) * self.d_head]
+    #             q, k, v = q_mapping(seq), k_mapping(seq), v_mapping(seq)
+
+    #             attention = self.softmax(q @ k.T / (self.d_head**0.5))
+    #             seq_result.append(attention @ v)
+    #         result.append(torch.hstack(seq_result))
+    #     return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
+
     def forward(self, sequences):
-        # Sequences has shape (N, seq_length, token_dim)
-        # We go into shape    (N, seq_length, n_heads, token_dim / n_heads)
-        # And come back to    (N, seq_length, item_dim)  (through concatenation)
-        result = []
-        for sequence in sequences:
-            seq_result = []
-            for head in range(self.n_heads):
-                q_mapping = self.q_mappings[head]
-                k_mapping = self.k_mappings[head]
-                v_mapping = self.v_mappings[head]
+        N, seq_len, _ = sequences.size()
 
-                seq = sequence[:, head * self.d_head: (head + 1) * self.d_head]
-                q, k, v = q_mapping(seq), k_mapping(seq), v_mapping(seq)
+        # apply linear transformation
+        Q = self.q_mapping(sequences)  # (N, seq_length, d)
+        K = self.k_mapping(sequences)  # (N, seq_length, d)
+        V = self.v_mapping(sequences)  # (N, seq_length, d)
 
-                attention = self.softmax(q @ k.T / (self.d_head ** 0.5))
-                seq_result.append(attention @ v)
-            result.append(torch.hstack(seq_result))
-        return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
+        # reshape to apply attention to all heads
+        Q = Q.view(N, seq_len, self.n_heads, self.d_head).transpose(
+            1, 2
+        )  # (N, n_heads, seq_len, d_head)
+        K = K.view(N, seq_len, self.n_heads, self.d_head).transpose(
+            1, 2
+        )  # (N, n_heads, seq_len, d_head)
+        V = V.view(N, seq_len, self.n_heads, self.d_head).transpose(
+            1, 2
+        )  # (N, n_heads, seq_len, d_head)
 
+        # compute scaled dot-product attention
+        attention = self.softmax(
+            Q @ K.transpose(-2, -1) / (self.d_head**0.5)
+        )  # (N, n_heads, seq_len, seq_len)
 
-    def forward_vectorized(self, sequences):
-        N, seq_length, token_dim = sequences.shape
-        qkv = self.qkv_mapping(sequences)  # Shape: (N, seq_length, 3 * d)
-        qkv = qkv.view(N, seq_length, self.n_heads, 3 * self.d_head)  # Shape: (N, seq_length, n_heads, 3 * d_head)
-        qkv = qkv.permute(0, 2, 1, 3)  # Transpose for attention computation: (N, n_heads, seq_length, 3 * d_head)
-        
-        q, k, v = torch.chunk(qkv, chunks=3, dim=-1)  # Split last dimension into q, k, v
-        q /= self.d_head ** 0.5  # Scale q
+        # apply attention weights to values
+        out = attention @ V  # (N, n_heads, seq_len, d_head)
 
-        attention = self.softmax(q @ k.transpose(-2, -1))  # Compute scaled dot-product attention
-        out = attention @ v  # Apply attention weights to v
-        out = out.permute(0, 2, 1, 3).contiguous()  # Back to original shape: (N, seq_length, n_heads, d_head)
-        out = out.view(N, seq_length, -1)  # Concatenate heads: (N, seq_length, d)
-        
+        # reshape output
+        out = out.transpose(1, 2).contiguous().view(N, seq_len, -1)  # (N, seq_len, d)
+
         return out
--- ViT Implementation/train.py
+++ ViT Implementation/train.py
@@ -1,66 +1,102 @@
-import torch 
+import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
 from torchvision.transforms import ToTensor
 from torchvision.datasets import CIFAR10
-from torchvision.datasets.mnist import MNIST
 from tqdm import tqdm, trange
+import argparse
 
 
 from model import ViT
 
 # Code is adapted from: https://medium.com/mlearning-ai/vision-transformers-from-scratch-pytorch-a-step-by-step-guide-96c3313c2e0c
-# Credit goes to Brian Pulfer 
+# Credit goes to Brian Pulfer
+
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="This takes in the transformer hparams"
+    )
+
+    parser.add_argument(
+        "--hidden_dim", "-d", type=int, required=True, help="hidden dimension size"
+    )
+    parser.add_argument(
+        "--n_heads", "-n", type=int, required=True, help="number of heads"
+    )
+    parser.add_argument(
+        "--n_patches", "-p", type=int, required=True, help="number of patches"
+    )
+    parser.add_argument(
+        "--n_blocks", "-b", type=int, required=True, help="number of patches"
+    )
+
+    args = parser.parse_args()
+    # n_patches=8, n_blocks=2, hidden_dim=8, n_heads=2, n_classes=10)
+
     # Loading data
     transform = ToTensor()
 
-    train_set = MNIST(root='./../datasets', train=True, download=True, transform=transform)
-    test_set = MNIST(root='./../datasets', train=False, download=True, transform=transform)
+    train_set = CIFAR10(
+        root="./../datasets", train=True, download=True, transform=transform
+    )
+    test_set = CIFAR10(
+        root="./../datasets", train=False, download=True, transform=transform
+    )
 
+    print(f"size of training set: {len(train_set)}")
+    print(f"size of test set: {len(test_set)}")
     train_loader = DataLoader(train_set, shuffle=True, batch_size=128)
     test_loader = DataLoader(test_set, shuffle=False, batch_size=128)
-
-    # train_set = CIFAR10(root='./../datasets', train=True, download=True, transform=transform)
-    # test_set = CIFAR10(root='./../datasets', train=False, download=True, transform=transform)
-
-    # print(f"size of training set: {len(train_set)}")
-    # print(f"size of test set: {len(test_set)}")
-    # train_loader = DataLoader(train_set, shuffle=True, batch_size=128)
-    # test_loader = DataLoader(test_set, shuffle=False, batch_size=128)
 
     image, _ = train_set[0]  # Get the first image from the dataset (ignoring the label)
     image_dims = image.shape
 
-
     # Defining model and training options
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("Using device: ", device, f"({torch.cuda.get_device_name(device)})" if torch.cuda.is_available() else "")
+    print(
+        "Using device: ",
+        device,
+        f"({torch.cuda.get_device_name(device)})" if torch.cuda.is_available() else "",
+    )
 
     # load the transformer model and move it to the GPU
-    model = ViT(image_dims, n_patches=8, n_blocks=2, hidden_dim=8, n_heads=2, n_classes=10).to(device)
+    model = ViT(
+        image_dims,
+        n_patches=args.n_patches,
+        n_blocks=args.n_blocks,
+        hidden_dim=args.hidden_dim,
+        n_heads=args.n_heads,
+        n_classes=10,
+    ).to(device)
 
-    N_EPOCHS = 5
-    LR = 0.005
+    N_EPOCHS = 1
+    LR = 0.01
 
     # Training loop
     optimizer = Adam(model.parameters(), lr=LR)
     criterion = CrossEntropyLoss()
     for epoch in trange(N_EPOCHS, desc="Training"):
         train_loss = 0.0
-        for batch in tqdm(train_loader, desc=f"Epoch {epoch + 1} in training", leave=False):
+        t_correct, t_total = 0, 0
+        for batch in tqdm(
+            train_loader, desc=f"Epoch {epoch + 1} in training", leave=False
+        ):
             x, y = batch
             x, y = x.to(device), y.to(device)
             y_hat = model(x)
             loss = criterion(y_hat, y)
-
             train_loss += loss.detach().cpu().item() / len(train_loader)
+            t_correct += (
+                torch.sum(torch.argmax(y_hat, dim=1) == y).detach().cpu().item()
+            )
+            t_total += len(x)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+        print(f"Train accuracy: {t_correct / t_total * 100:.2f}%")
 
         print(f"Epoch {epoch + 1}/{N_EPOCHS} loss: {train_loss:.2f}")
 
@@ -79,6 +115,11 @@ def main():
             total += len(x)
         print(f"Test loss: {test_loss:.2f}")
         print(f"Test accuracy: {correct / total * 100:.2f}%")
+
+    torch.save(model.state_dict(), "model_path.pt")
+
+    for name, module in model.named_modules():
+        print(name)
 
 
 if __name__ == "__main__":
