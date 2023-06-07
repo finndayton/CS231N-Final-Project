462b4e10321b290bd23ae5d40b3f1e0c6a21b68d
Ubuntu <ubuntu@ip-172-31-7-94.us-west-2.compute.internal>
2023-06-07 02:58:22 +0000

    model running on imagenet

--- ViT_Implementation/model.py
+++ ViT_Implementation/model.py
@@ -13,7 +13,7 @@ from utils import *
 
 
 class ViT(nn.Module):
-    def __init__(self, chw = (3, 224, 224), n_patches = 8, n_blocks=4, hidden_dim = 16, n_heads=4, n_classes=10):
+    def __init__(self, chw = (3, 224, 224), n_patches = 8, n_blocks=4, hidden_dim = 16, n_heads=4, n_classes=1000):
         super(ViT, self).__init__()
         self.chw = chw
         self.n_patches = n_patches
--- ViT_Implementation/train.py
+++ ViT_Implementation/train.py
@@ -2,12 +2,13 @@ import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, Lambda
 from torchvision.datasets import CIFAR10, ImageNet
 from torchvision.transforms import Compose, Resize, ToTensor
 from datasets import load_dataset
 from tqdm import tqdm, trange
 import argparse
+import random
 
 
 from model import ViT
@@ -39,25 +40,46 @@ def main():
 
     # Loading data
     # transform = ToTensor()
-    transform = Compose([Resize((224, 224)), ToTensor()])
+    transform = Compose([
+        Resize((224, 224)),
+        Lambda(lambda x: x.convert('RGB')),
+        ToTensor()
+    ])
 
-    # train_set = ImageNet(
-    #     root="./../datasets", train=True, download=True, transform=transform
-    # )
-    # test_set = ImageNet(
-    #     root="./../datasets", train=False, download=True, transform=transform
-    # )
+    def apply_transform(example):
+        #print(type(example['image']))
+        example['image'] = transform(example['image'])
+        #print(type(example['image']))
+        return example
+
+    
     dataset = load_dataset("imagenet-1k")
-    return
+    # print(dataset)
 
+    # Get random indices from the range 0 to len(dataset)
+    random_indices_train = random.sample(range(len(dataset['train'])), 100000)
+    random_indices_test = random.sample(range(len(dataset['train'])), 5000)
+    
+    # Select random samples from the train set
+    train_set = dataset['train'].select(random_indices_train).map(apply_transform)
+    test_set = dataset['train'].select(random_indices_test).map(apply_transform)
+    
+    train_set.set_format(type='torch', columns=['image', 'label'])
+    test_set.set_format(type='torch', columns=['image', 'label'])
+    
+    #print(train_set)
+    
     print(f"size of training set: {len(train_set)}")
     print(f"size of test set: {len(test_set)}")
     train_loader = DataLoader(train_set, shuffle=True, batch_size=128)
-    test_loader = DataLoader(test_set, shuffle=False, batch_size=128)
+    test_loader = DataLoader(test_set, shuffle=True, batch_size=128)
 
-    image, _ = train_set[0]  # Get the first image from the dataset (ignoring the label)
+    print(train_set[0])
+    print(test_set[0])
+    image = train_set[0]['image']  # Get the first image from the dataset (ignoring the label)
     image_dims = image.shape
-
+    print(image_dims)
+    
     # Defining model and training options
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(
@@ -73,7 +95,7 @@ def main():
         n_blocks=args.n_blocks,
         hidden_dim=args.hidden_dim,
         n_heads=args.n_heads,
-        n_classes=10,
+        n_classes=1000,
     ).to(device)
 
 
@@ -95,7 +117,9 @@ def main():
         for batch in tqdm(
             train_loader, desc=f"Epoch {epoch + 1} in training", leave=False
         ):
-            x, y = batch
+            x = batch['image']
+            y = batch['label']
+            print("train", y)
             x, y = x.to(device), y.to(device)
             y_hat = model(x)
             loss = criterion(y_hat, y)
@@ -117,7 +141,9 @@ def main():
         correct, total = 0, 0
         test_loss = 0.0
         for batch in tqdm(test_loader, desc="Testing"):
-            x, y = batch
+            x = batch['image']
+            y = batch['label']
+            print("test", y)
             x, y = x.to(device), y.to(device)
             y_hat = model(x)
             loss = criterion(y_hat, y)
@@ -128,7 +154,7 @@ def main():
         print(f"Test loss: {test_loss:.2f}")
         print(f"Test accuracy: {correct / total * 100:.2f}%")
     # save the model to disk
-    torch.save(model.state_dict(), "model.pth")
+    torch.save(model.state_dict(), "mymodel.pth")
 
 if __name__ == "__main__":
     main()
